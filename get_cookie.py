8c0e230f9374e257c2b7e528a3dc16eaa019c411
gonaxx <113344038+gonaxx@users.noreply.github.com>
2023-05-06 04:40:39 +0800

    Update get_cookie.py

--- get_cookie.py
+++ get_cookie.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import json
+import time
 
 browser_options = Options()
 browser = webdriver.Chrome(chrome_options=browser_options)
